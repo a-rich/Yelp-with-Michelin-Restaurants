8a9312ec63a5fb7cabfabe4c26f7503972e1f494
Alex <alexrichards92@me.com>
2017-05-01 00:09:15 -0700

    Nearly finished wrapper for database

--- web-app/wrapper.py
+++ web-app/wrapper.py
@@ -4,6 +4,7 @@ from sqlite3 import Error
 
 class MichelinStarredAPI:
   __conn = None
+  suggestions = None
 
   """
     Connects to the database named in the parameter, fetches the maximum ID in
@@ -13,46 +14,91 @@ class MichelinStarredAPI:
     try:
       self.__conn = sqlite3.connect(database)
       cursor = self.__conn.cursor()
-      cursor.execute("""PRAGMA foreign_keys = ON""").close()
-    except Error as e:
-      print(e)
-
-  """
-  """
-  def queryPublication(self, record, exact=True, output_format='JSON',
-      sorted_order='title', reverse=False, queryRange="0,50"):
-    author, title, year, journal = record[0], record[1], record[2], record[3]
-    start, end = queryRange.split(',')[0], queryRange.split(',')[1]
-
-    cond1 = '' if not author else "lower(a.name) = '{0}'".format(author.lower())
-    cond2 = '' if not title else "lower(p.title) = '{0}'".format(title.lower())
-    cond3 = '' if not year else "p.year = {0}".format(year)
-
-    cond1 = cond1 + " AND " if cond1 and (cond2 or cond3 or cond4) else cond1
-    cond2 = cond2 + " AND " if cond2 and (cond3 or cond4 ) else cond2
-    cond3 = cond3 + " AND " if cond3 and cond4 else cond3
-    cond5 = "AND " if cond1 or cond2 or cond3 or cond4 else ""
-    cond6 = "ORDER BY " + sorted_order + " DESC" if reverse else "ORDER BY " + sorted_order
-
-    sql_pubs = """SELECT DISTINCT p.id, p.title, p.year, p.booktitle
-                  FROM publication as p, written_by as w, author as a
-                  WHERE p.id = w.pub_id and w.author_id = a.id
-                  {4} {0}{1}{2}{3} {5} LIMIT {6},{7};
-              """.format(cond1, cond2, cond3, cond4, cond5, cond6, start, end)
-
-    sql_authors = """SELECT p.id, a.name
-                     FROM publication as p, written_by as w, author as a
-                     WHERE p.id = w.pub_id and w.author_id = a.id
-                     {4} {0}{1}{2}{3} {5};
-                  """.format(cond1, cond2, cond3, cond4, cond5, cond6)
-
-    try:
-      cursor = self.__conn.cursor()
-      result_pubs = cursor.execute(sql_pubs).fetchall()
-      result_authors = cursor.execute(sql_authors).fetchall()
+      sql = """
+              SELECT name, 'city' FROM city
+              UNION SELECT name, 'state' FROM state
+              UNION SELECT name, 'restaurant' FROM restaurant
+              UNION SELECT title, 'category' FROM category;
+            """
+      self.suggestions = {r[0]: r[1] for r in cursor.execute(sql).fetchall()}
       cursor.close()
     except Error as e:
       print(e)
 
+  def getByCity(self, c):
+    try:
+      cursor = self.__conn.cursor()
+      sql = """
+              SELECT name FROM restaurant
+              WHERE city = '{0}'
+              ORDER BY name;
+            """.format(c)
+      results = [r[0] for r in cursor.execute(sql).fetchall()]
+      cursor.close()
+      return results
+    except Error as e:
+      print(e)
+
+  def getByState(self, s):
+    try:
+      cursor = self.__conn.cursor()
+      sql = """
+              SELECT name, city FROM restaurant
+              WHERE state = '{0}'
+              ORDER BY city, name;
+            """.format(s)
+      results = [' '.join([r[0]+',', r[1]]) for r in cursor.execute(sql).fetchall()]
+      cursor.close()
+      return results
+    except Error as e:
+      print(e)
+
+  def getByCategory(self, c):
+    try:
+      cursor = self.__conn.cursor()
+      sql = """
+              SELECT name, city, state FROM restaurant
+              WHERE id IN (
+                SELECT restaurant_id FROM restaurant_by_category, category
+                WHERE category_id = id and title = '{0}'
+              )
+              ORDER BY state, city, name;
+            """.format(c)
+      results = [' '.join([r[0]+',', r[1]+',', r[2]]) for r in cursor.execute(sql).fetchall()]
+      cursor.close()
+      return results
+    except Error as e:
+      print(e)
+
+  def getByRestaurant(self, r):
+    try:
+      cursor = self.__conn.cursor()
+      sql = """
+              SELECT * FROM restaurant
+              WHERE name = "{0}";
+            """.format(r)
+      restaurant = cursor.execute(sql).fetchall()[0]
+      sql = """
+              SELECT * FROM review
+              WHERE restaurant_id = {0};
+            """.format(restaurant[0])
+      reviews = cursor.execute(sql).fetchall()
+      result = (restaurant, reviews)
+      cursor.close()
+      return result
+    except Error as e:
+      print(e)
+
+  def triggerQuery(self, word):
+    if self.suggestions[word] == 'city':
+      return getByCity(word)
+    elif self.suggestions[word] == 'state':
+      return getByState(word)
+    elif self.suggestions[word] == 'category':
+      return getByCategory(word)
+    else:
+      return getByRestaurant(word)
+
 if __name__ == '__main__':
   api = MichelinStarredAPI('../data_processing/database.db')
+
