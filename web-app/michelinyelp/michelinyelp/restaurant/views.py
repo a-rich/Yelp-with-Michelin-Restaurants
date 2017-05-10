43d6be8072be3e1e69d8bf9e6b6a88aff0b92e60
Alex <alexrichards92@me.com>
2017-05-10 01:45:51 -0700

    1.0-alpha

--- web-app/michelinyelp/michelinyelp/restaurant/views.py
+++ web-app/michelinyelp/michelinyelp/restaurant/views.py
@@ -25,15 +25,35 @@ class SearchView(FormView):
         return self.render_to_response(ctx)
 
 
+class RestaurantAllView(DetailView):
+    model = Restaurant
+    template_name = 'restaurant_all.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantAllView, self).get_context_data(**kwargs)
+        context['restaurants'] = Restaurant.objects.all()
+        return context
+
+
 class RestaurantDetailView(DetailView):
     model = Restaurant
     template_name = 'restaurant_detail.html'
 
     def get_context_data(self, **kwargs):
-      context = super(RestaurantDetailView, self).get_context_data(**kwargs)
-      context['reviews'] = Review.objects.filter(restaurant_id=self.object.id)
-      self.object.url = ':'.join(['https'] + self.object.url.split('https')[1:])
-      return context
+        context = super(RestaurantDetailView, self).get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(restaurant_id=self.object.id)
+        self.object.url = ':'.join(['https'] + self.object.url.split('https')[1:])
+        return context
+
+
+class CityAllView(DetailView):
+    model = City
+    template_name = 'city_all.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CityAllView, self).get_context_data(**kwargs)
+        context['cities'] = City.objects.all()
+        return context
 
 
 class CityListView(DetailView):
@@ -41,9 +61,29 @@ class CityListView(DetailView):
     template_name = 'city_detail.html'
 
 
+class StateAllView(DetailView):
+    model = State
+    template_name = 'state_all.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(StateAllView, self).get_context_data(**kwargs)
+        context['states'] = State.objects.all()
+        return context
+
+
 class StateListView(DetailView):
     model = State
     template_name = 'state_detail.html'
+
+
+class CategoryAllView(DetailView):
+    model = Category
+    template_name = 'category_all.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryAllView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class CategoryListView(DetailView):
@@ -51,9 +91,9 @@ class CategoryListView(DetailView):
     template_name = 'category_detail.html'
 
     def get_context_data(self, **kwargs):
-      context = super(CategoryListView, self).get_context_data(**kwargs)
-      restaurants_by_category = RestaurantByCategory.objects.filter(category_id=self.object)
-      restaurant_ids = [r.restaurant_id.id for r in restaurants_by_category]
-      context['restaurants'] = Restaurant.objects.filter(id__in=restaurant_ids)
-      return context
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        restaurants_by_category = RestaurantByCategory.objects.filter(category_id=self.object)
+        restaurant_ids = [r.restaurant_id.id for r in restaurants_by_category]
+        context['restaurants'] = Restaurant.objects.filter(id__in=restaurant_ids)
+        return context
 
